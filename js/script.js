528d780792fc382a46818c917b9c256905018eaa
Izan Estirado Alfaro <40562916+AstroIzan@users.noreply.github.com>
2024-02-19 17:36:38 +0100

    update

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function notificar() {
         icon: 'src/icon.png',
         timeout: 4000,
         vibrate: [200, 100, 200, 100, 200, 100, 200],
+        style: 'border-bottom: 5px solid #FFD700;',
         onClick: function () {
             console.log(this);
         }
